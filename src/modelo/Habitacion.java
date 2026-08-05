55cf36ce902f9b1e89ecfa5c6001ff689d7730be
SachaCoggiola <scbalgane@gmail.com>
2026-08-05 19:13:14 -0300

    CRUD de desafios en habitacion y sistema, metodo caminoMenorCosto grafo

--- src/modelo/Habitacion.java
+++ src/modelo/Habitacion.java
@@ -63,4 +63,53 @@ public class Habitacion {
     public boolean equals(Habitacion habitacion) {
         return habitacion.codigo == this.codigo;
     }
+
+    //CRUD Desafios
+    //Create
+    public boolean agregarDesafio(int puntaje, String nombre, String tipo) {
+        return this.desafios.insertar(puntaje,new Desafio(puntaje,nombre,tipo));    //Crear un nuevo Desafio y lo añade al diccionario de la Habitacion
+    }
+    //Read
+    public String obtenerNombreDesafio(int puntaje){
+        String nombre = "Desafio no encontrado";
+        Desafio buscado = (Desafio) this.desafios.obtenerInfo(puntaje); //Busca el desafio que tiene la clave ingresada
+        if(buscado != null){                    //Pregunta si se encontro el desafio
+            nombre = buscado.getNombre();
+        }
+        return nombre;
+    }
+    public String obtenerTipoDesafio(int puntaje){
+        String nombre = "Desafio no encontrado";
+        Desafio buscado = (Desafio) this.desafios.obtenerInfo(puntaje); //Busca el desafio que tiene la clave ingresada
+        if(buscado != null){                    //Pregunta si se encontro el desafio
+            nombre = buscado.getTipo();
+        }
+        return nombre;
+    }
+    public Desafio getDesafio(int puntaje){
+        return (Desafio) this.desafios.obtenerInfo(puntaje);
+    }
+    //Update
+    public boolean cambiarNombreDesafio(int puntaje, String nombreDesafio) {
+        boolean exito = false;                  //Si no se encuentra el desafio, devuelve false
+        Desafio buscado = (Desafio) this.desafios.obtenerInfo(puntaje); //Busca el desafio que tiene la clave ingresada
+        if(buscado != null){                    //Pregunta si se encontro el desafio
+            buscado.setNombre(nombreDesafio);   //Si se encontro, le cambia el nombre
+            exito = true;                       //Devuelve true
+        }
+        return exito;
+    }
+    public boolean cambiarTipoDesafio(int puntaje, String tipoDesafio) {
+        boolean exito = false;                  //Si no se encuentra el desafio, devuelve false
+        Desafio buscado = (Desafio) this.desafios.obtenerInfo(puntaje); //Busca el desafio que tiene la clave ingresada
+        if(buscado != null){                    //Pregunta si se encontro el desafio
+            buscado.setNombre(tipoDesafio);   //Si se encontro, le cambia el nombre
+            exito = true;                       //Devuelve true
+        }
+        return exito;
+    }
+    //Delete
+    public boolean sacarDesafio(int puntaje) {
+        return this.desafios.eliminar(puntaje);
+    }
 }
--- src/sistema/SistemaEscapeHouse.java
+++ src/sistema/SistemaEscapeHouse.java
@@ -28,24 +28,159 @@ public class SistemaEscapeHouse {
     //CRUD Habitaciones
 
     //CRUD Desafíos
+    private boolean verificarPuntaje(int puntaje){
+        return puntaje >= 0 && puntaje <= 100;
+    }
+    private boolean verificarString(String cadena){
+        return cadena != null && !cadena.isEmpty();
+    }
+    private boolean verificarCodigo(int codigo){
+        return codigo >= 0 && codigo <= 25;
+    }
+    //Create
+    public String agregarDesafio(int codigoHab, int puntaje, String nombre, String tipo) {
+        String cadena = "Los datos ingresados no cumplen con el formato deseado";
+        if(verificarCodigo(codigoHab)&&verificarPuntaje(puntaje)&&verificarString(nombre)&&verificarString(tipo)) {
+            cadena = "Habitacion no encontrada";
+            Habitacion habitacion = (Habitacion) this.habitaciones.obtenerInfo(codigoHab);
+            if (habitacion != null) {
+                if (habitacion.agregarDesafio(puntaje, nombre, tipo)) {
+                    cadena = "Desafio agregado correctamente";
+                } else {
+                    cadena = "No se ha podido agregar el desafio";
+                }
+            }
+        }
+        return cadena;
+    }
+    //Read
+    public String obtenerNombreDesafio(int codigoHab, int puntaje){
+        String cadena = "Los datos ingresados no cumplen con el formato deseado";
+        if(verificarCodigo(codigoHab)&&verificarPuntaje(puntaje)) {
+            cadena = "Habitacion no encontrada";
+            Habitacion habitacion = (Habitacion) this.habitaciones.obtenerInfo(codigoHab);
+            if (habitacion != null) {
+                cadena = habitacion.obtenerNombreDesafio(puntaje);
+            }
+        }
+        return cadena;
+    }
+    public String obtenerTipoDesafio(int codigoHab, int puntaje){
+        String cadena = "Los datos ingresados no cumplen con el formato deseado";
+        if(verificarCodigo(codigoHab)&&verificarPuntaje(puntaje)) {
+            cadena = "Habitacion no encontrada";
+            Habitacion habitacion = (Habitacion) this.habitaciones.obtenerInfo(codigoHab);
+            if (habitacion != null) {
+                cadena = habitacion.obtenerTipoDesafio(puntaje);
+            }
+        }
+        return cadena;
+    }
+    //Update
+    public String cambiarNombreDesafio(int codigoHab, int puntaje, String nombreDesafio) {
+        String cadena = "Los datos ingresados no cumplen con el formato deseado";
+        if(verificarCodigo(codigoHab)&&verificarPuntaje(puntaje)&&verificarString(nombreDesafio)) {
+            cadena = "Habitacion no encontrada";
+            Habitacion habitacion = (Habitacion) this.habitaciones.obtenerInfo(codigoHab);
+            if (habitacion != null) {
+                if (habitacion.cambiarNombreDesafio(puntaje, nombreDesafio)) {
+                    cadena = "Se ha cambiado exitosamente el nombre del desafio";
+                } else {
+                    cadena = "No se ha podido cambiar el nombre del desafio";
+                }
+            }
+        }
+        return cadena;
+    }
+    public String cambiarTipoDesafio(int codigoHab, int puntaje, String tipoDesafio) {
+        String cadena = "Los datos ingresados no cumplen con el formato deseado";
+        if(verificarCodigo(codigoHab)&&verificarPuntaje(puntaje)&&verificarString(tipoDesafio)) {
+            cadena = "Habitacion no encontrada";
+            Habitacion habitacion = (Habitacion) this.habitaciones.obtenerInfo(codigoHab);
+            if (habitacion != null) {
+                if (habitacion.cambiarTipoDesafio(puntaje, tipoDesafio)) {
+                    cadena = "Se ha cambiado exitosamente el tipo del desafio";
+                } else {
+                    cadena = "No se ha podido cambiar el tipo del desafio";
+                }
+            }
+        }
+        return cadena;
+    }
+    //Delete
+    public String sacarDesafio(int codigoHab, int puntaje) {
+        String cadena = "Los datos ingresados no cumplen con el formato deseado";
+        if (verificarCodigo(codigoHab)&&verificarPuntaje(puntaje)) {
+            cadena = "Habitacion no encontrada";
+            Habitacion habitacion = (Habitacion) this.habitaciones.obtenerInfo(codigoHab);
+            if (habitacion != null) {
+                if (habitacion.sacarDesafio(puntaje)) {
+                    cadena = "Se ha sacado exitosamente el desafio";
+                }else{
+                    cadena = "No se ha podido sacar el desafio";
+                }
+            }
+        }
+        return cadena;
+    }
 
     // CRUD Equipos
 
     // consultas sobre habitaciones
     public String mostrarHabitacion(int codigo){
-        Habitacion aux = (Habitacion) this.habitaciones.obtenerInfo(codigo);
-        return aux.toString();
+        String cadena = "Habitación no encontrada";
+        Habitacion buscado = (Habitacion) this.habitaciones.obtenerInfo(codigo);
+        if(buscado != null){
+            cadena = buscado.toString();
+        }
+        return cadena;
     }
 
     public String minimoPuntaje(int cod1, int cod2){
         Habitacion hab1 = (Habitacion) this.habitaciones.obtenerInfo(cod1);
         Habitacion hab2 = (Habitacion) this.habitaciones.obtenerInfo(cod2);
-        return this.planoCasa.caminoMasCorto(hab1,hab2).toString() + "\n El puntaje requerido para pasar de la habitacion: "+hab1.getNombre()+" a la habitacion: "+hab2.getNombre()+ "es de: ";
+        StringBuilder cadena = new StringBuilder();
+        cadena.append(this.planoCasa.caminoMasCorto(hab1,hab2).toString());
+        cadena.append("\nEl puntaje requerido para pasar de la habitacion: ");
+        cadena.append(hab1.getNombre()).append(" a la habitacion: ");
+        cadena.append(hab2.getNombre()).append("es de: ");
+        cadena.append(this.planoCasa.caminoMenorCosto(hab1,hab2));
+        return cadena.toString();
     }
 
     //consultas sobre desafíos
+    public String mostrarDesafiosResueltos(String nombreEquipo){
+        String resueltos = "Equipo no encontrado";
+        Equipo buscado = (Equipo) this.equipos.obtenerInfo(nombreEquipo);
+        if(buscado != null){
+            //resueltos = buscado.listarDesafios().toString();
+        }
+        return resueltos;
+    }
 
     //consultas sobre equipos
+    public boolean jugarDesafio(String nombreEquipo, int codigoHab, int puntaje){
+        Equipo equipo = (Equipo) this.equipos.obtenerInfo(nombreEquipo);
+        Habitacion habitacion;
+        Desafio desafio;
+        Lista aux;
+        boolean exito = false;
+        if(equipo != null){
+            habitacion = (Habitacion) this.habitaciones.obtenerInfo(codigoHab);
+            if(habitacion != null){
+                desafio = habitacion.getDesafio(puntaje);
+                if(desafio != null){
+                    equipo.acumularPuntaje(puntaje);
+                    equipo.acumularPuntajeEnHabitacion(puntaje);
+                    aux = this.desafiosResueltosPorEquipo.get(nombreEquipo);
+                    aux.insertar(desafio, aux.longitud()+1);
+                    this.desafiosResueltosPorEquipo.put(nombreEquipo,aux);
+                    exito = true;
+                }
+            }
+        }
+        return exito;
+    }
 
     //consultas generales
 }
